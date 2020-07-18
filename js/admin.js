72da965b8b642cafc5ca1baecc53eafa6b5e2283
unknown <fionaodoboh@gmail.com>
2020-07-18 22:23:08 +0100

    create product modal added

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,8 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const dashboardItems = document.querySelector(".small-flex-container");
+const openModal = document.querySelector(".modal");
+const closeModal = document.querySelector(".close");
+const modalContainer = document.querySelector(".create-container");
 
 menuToggle.addEventListener("click", () => {
   dashboardItems.classList.toggle("open");
@@ -7,4 +10,23 @@ menuToggle.addEventListener("click", () => {
 
 menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("change");
+});
+
+
+//Create products Modal
+
+//open modal
+openModal.addEventListener("click", () => {
+  modalContainer.classList.add("open-modal");
+});
+
+//close modal
+
+closeModal.addEventListener("click", () => {
+
+  if (modalContainer.classList.contains("open-modal")) {
+    modalContainer.classList.remove("open-modal");
+    // modalContainer.classList.add("remove-modal");
+  }
+
 });
